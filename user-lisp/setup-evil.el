46824aa8edcb6c0ec7e47b3e98f547fddfc19608
Max Veytsman <maxim@ontoillogical.com>
2015-01-11 05:09:51 -0500

    evil mode

---
+++ user-lisp/setup-evil.el
@@ -0,0 +1,23 @@
+(require 'evil)
+
+(require 'evil-surround)
+(global-evil-surround-mode 1)
+
+(require 'evil-visualstar)
+
+;; these modes are clear from evil
+(add-hook 'term-mode-hook 'evil-emacs-state)
+(add-hook 'prodigy-mode-hook 'evil-emacs-state)
+(add-hook 'cider-repl-mode-hook 'evil-emacs-state)
+
+;; evil messes up emacs undo
+(global-undo-tree-mode 0)
+
+;; I prefer having jump-to-tag bound to M-.
+(define-key evil-normal-state-map (kbd "M-.") nil)
+
+(setq evil-search-module 'evil-search
+      evil-want-C-u-scroll t
+      evil-want-C-w-in-emacs-state t)
+(evil-mode 1)
+(provide 'setup-evil)
