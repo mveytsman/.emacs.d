b28381463c201c9131a9f6136364eab8ec4c3fb7
Max Veytsman <maxim@ontoillogical.com>
2015-01-12 11:26:36 -0500

    More tweaks of company bindings

--- user-lisp/setup-company-mode.el
+++ user-lisp/setup-company-mode.el
@@ -1,10 +1,22 @@
 (require 'company)
 
+(defun company-complete-common-or-selection ()
+  "Insert the common part of all candidates, or the selection."
+  (interactive)
+  (when (company-manual-begin)
+    (let ((tick (buffer-chars-modified-tick)))
+      (call-interactively 'company-complete-common)
+      (when (eq tick (buffer-chars-modified-tick))
+        (let ((company-selection-wrap-around t))
+          (call-interactively 'company-complete-selection))))))
+
+
+
 (define-key company-active-map (kbd "RET") nil)
 (define-key company-active-map [return] nil)
 (define-key company-active-map (kbd "ESC") 'company-abort)
-(define-key company-active-map [tab] 'company-complete-selection)
-(define-key company-active-map (kbd "TAB") 'company-complete-selection)
+(define-key company-active-map [tab] 'company-complete-common-or-selection)
+(define-key company-active-map (kbd "TAB") 'company-complete-common-or-selection)
 (setq company-idle-delay 0.125
       company-minimum-prefix-length 3
       company-require-match nil
