595b05e1efa3e98c67cbf30fcfda5644a67a327b
Max Veytsman <maxim@ontoillogical.com>
2015-01-12 11:26:17 -0500

    Evil bindings

--- user-lisp/setup-evil.el
+++ user-lisp/setup-evil.el
@@ -1,10 +1,11 @@
-(require 'evil)
+ (require 'evil)
 
 (require 'evil-surround)
 (global-evil-surround-mode 1)
 
 (require 'evil-visualstar)
 
+(evilnc-default-hotkeys)
 ;; these modes are clear from evil
 (add-hook 'term-mode-hook 'evil-emacs-state)
 (add-hook 'prodigy-mode-hook 'evil-emacs-state)
@@ -12,6 +13,13 @@
 
 ;; evil messes up emacs undo
 (global-undo-tree-mode 0)
+
+
+;; Copy and paste
+(define-key evil-normal-state-map (kbd "s-c") 'evil-yank)
+(define-key evil-normal-state-map (kbd "s-p") 'evil-paste-after)
+
+
 
 ;; I prefer having jump-to-tag bound to M-.
 (define-key evil-normal-state-map (kbd "M-.") nil)
