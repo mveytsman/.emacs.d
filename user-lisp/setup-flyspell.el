670c56279ffe993990914f40a79ec56709b7544c
Max Veytsman <maxim@ontoillogical.com>
2015-01-13 02:37:00 -0500

    Flyspell

---
+++ user-lisp/setup-flyspell.el
@@ -0,0 +1,14 @@
+;; find aspell and hunspell automatically
+(cond
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_CA")))
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-really-hunspell t)
+  (setq ispell-extra-args '("-d en_CA")))
+ )
+
+(add-hook 'text-mode-hook 'flyspell-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+(provide 'setup-flyspell)
