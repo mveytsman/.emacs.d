a4dd550275e719900b3fd1b5ebccc723b13213b1
Max Veytsman <mveytsman@github.com>
2018-07-16 08:40:32 -0400

    Custom.el

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (robe projectile-rails go-mode dockerfile-mode csv-mode slime-company slime whitespace-cleanup-mode ag flycheck magit counsel-projectile counsel ivy company expand-region rich-minority comment-dwim-2 dashboard unkillable-scratch exec-path-from-shell use-package))))
+    (neotree robe projectile-rails go-mode dockerfile-mode csv-mode slime-company slime whitespace-cleanup-mode ag flycheck magit counsel-projectile counsel ivy company expand-region rich-minority comment-dwim-2 dashboard unkillable-scratch exec-path-from-shell use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
