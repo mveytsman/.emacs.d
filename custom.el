6b412b664555d675957d296b55c53840f59634f1
Max Veytsman <mveytsman@github.com>
2018-08-24 21:03:57 -0400

    custom.el

--- custom.el
+++ custom.el
@@ -5,7 +5,17 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (neotree robe projectile-rails go-mode dockerfile-mode csv-mode slime-company slime whitespace-cleanup-mode ag flycheck magit counsel-projectile counsel ivy company expand-region rich-minority comment-dwim-2 dashboard unkillable-scratch exec-path-from-shell use-package))))
+    (visual-regexp amx popwin smex cider markdown-mode ace-window shell-pop flycheck-credo flycheck-elixir-credo alchemist elixir-mode deft libgit neotree robe projectile-rails go-mode dockerfile-mode csv-mode slime-company slime whitespace-cleanup-mode ag flycheck magit counsel-projectile counsel ivy company expand-region rich-minority comment-dwim-2 dashboard unkillable-scratch exec-path-from-shell use-package)))
+ '(shell-pop-default-directory "~/")
+ '(shell-pop-full-span t)
+ '(shell-pop-shell-type
+   (quote
+    ("eshell" "*eshell*"
+     (lambda nil
+       (mveytsman/ansi-term)))))
+ '(shell-pop-universal-key "C-'")
+ '(shell-pop-window-position "bottom")
+ '(shell-pop-window-size 30))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
